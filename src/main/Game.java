d52034b8a15bccffa8bfc4d8e13625f42fe44876
LOTHSAVAN hugo <hugo.lothsavan@etu.u-paris.fr>
2024-01-03 00:11:51 +0100

    update Fps et ups

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,9 +6,14 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements Runnable{
     private GameScreen gameScreen;
-    private BufferedImage img; 
+    private BufferedImage image; 
+    private int Updates;
+    private long LastTimeUPS;
+    private Thread gameThread;
+    private final double FPSGame = 120.0;
+    private final double UPSGame = 60.0;
 
     public Game(){
         
@@ -18,7 +23,7 @@ public class Game extends JFrame{
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen(img);
+        gameScreen = new GameScreen(image);
         add(gameScreen);
     }
 
@@ -27,17 +32,60 @@ public class Game extends JFrame{
 		InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
 
 		try {
-			img = ImageIO.read(is);
+			image = ImageIO.read(is);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+    }
 
-	}
+    private void start(){
+        gameThread = new Thread(this){};
+        gameThread.start();
+    }
+
 
     public static void main(String[] args){
         
         SwingUtilities.invokeLater(() -> {
             Game game = new Game();
+            game.start();
         });
+        
+        
+    }
+
+    @Override
+    public void run() {
+        
+        double TimeUpdate = 1000000000.0 / UPSGame;
+        double TimeFPS = 1000000000.0 / FPSGame;
+        
+        long LastFPS=System.nanoTime();
+        long LastUpdate = System.nanoTime();
+        long LastTimeVerif = System.currentTimeMillis();
+       
+        int FPS =0;
+        int Updates = 0;
+
+        long current;
+        
+        while(true){
+            current = System.nanoTime();
+            if(current - LastFPS >= TimeFPS){
+                repaint();
+                LastFPS = current;
+                FPS++;
+            }
+            if(current - LastUpdate >= TimeUpdate){
+                LastUpdate = current;
+                Updates++;
+            }
+            if(System.currentTimeMillis()-LastTimeVerif >= 1000){
+                System.out.println("FPS : " + FPS + " | UPS : " + Updates);
+                FPS = 0;
+                Updates =0;
+                LastTimeVerif = System.currentTimeMillis();
+            }
+        }
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -4,23 +4,25 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
 import java.awt.image.BufferedImage;
+import java.sql.Time;
 
 public class GameScreen extends JPanel{
     private Random random;
-    private BufferedImage img;
+    private BufferedImage image;
     private ArrayList<BufferedImage> sprites = new ArrayList<BufferedImage>();
 
-    public GameScreen(BufferedImage img){
-        this.img = img;
-        loadSprites();
+
+    public GameScreen(BufferedImage image){
+        this.image = image;
+        AfficheSprites();
 
         random = new Random();
     }
     
-    private void loadSprites() {
+    private void AfficheSprites() {
         for(int y = 0; y < 10; y++){
             for(int x = 0; x < 10; x++){
-                sprites.add(img.getSubimage(x*32, y*32, 32, 32));
+                sprites.add(image.getSubimage(x*32, y*32, 32, 32));
                 
             }
         }
@@ -40,8 +42,10 @@ public class GameScreen extends JPanel{
                 g.drawImage(sprites.get(getRndInt()), x*32, y*32, null);
             }
         }
-        
+
     }
+
+    
 
     private int getRndInt(){
         return random.nextInt(100);
